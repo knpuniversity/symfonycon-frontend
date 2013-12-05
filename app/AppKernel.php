6eb52826cfbe45b1f0fe4e975c73c418e7969f8f
Ryan Weaver <ryan@thatsquality.com>
2013-12-04 20:44:26 -0500

    Adding a new staging directory that looks at the assets-built

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -26,7 +26,7 @@ class AppKernel extends Kernel
             new Yoda\UserBundle\UserBundle(),
         );
 
-        if (in_array($this->getEnvironment(), array('dev', 'test'))) {
+        if (in_array($this->getEnvironment(), array('dev', 'test', 'staging'))) {
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
